77ce0844a5ae05fac8e0e113113955d098b81ff4
James L. Walsh <jloubierwalsh@gmail.com>
2019-03-14 10:28:59 -0400

    Add support for async factories

--- src/FactoryProviderBuilder.ts
+++ src/FactoryProviderBuilder.ts
@@ -1,7 +1,7 @@
 import { FactoryProvider, Type } from "@nestjs/common/interfaces";
 import { Providable } from "./ProviderBuilder";
 
-export type ProviderFactory<T> = (...args: any[]) => T;
+export type ProviderFactory<T> = (...args: any[]) => T | Promise<T>;
 type Injection = Type<any> | string | any;
 
 export default class FactoryProviderBuilder<T> {
